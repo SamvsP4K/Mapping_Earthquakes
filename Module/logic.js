cd2654f826c01675bae9078a970f34972c1e92b6
SamvsP4K <soundsofsamvs@gmail.com>
2022-12-10 16:22:31 -0800

    added an array of cities to the map

--- Module/logic.js
+++ Module/logic.js
@@ -11,11 +11,21 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 });
 
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color:"black",
-    fillColor: '#ffffa1'
- }).addTo(map);
+// Get data from cities.js
+let cityData = cities;
+
+  // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city),
+    L.circleMarker(city.location,{
+        radius: city.population/200000,
+        color: "orange"
+    })
+    .bindPopup("<h2>" + city.city +"," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map)
+   });
+
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
