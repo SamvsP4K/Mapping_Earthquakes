2777ce108329665c4e9a2cb810a43ce9ecd93033
SamvsP4K <soundsofsamvs@gmail.com>
2022-12-10 16:45:58 -0800

    added lines to city airports

--- Module/logic.js
+++ Module/logic.js
@@ -2,10 +2,24 @@
 console.log("working");
 
 //Create map object
-let map = L.map('mapid').setView([40.7,-94.5],4);
+let map = L.map('mapid').setView([36.1733, -120.1794],7);
+
+//coordinates to be used for line
+let line = [
+    [33.9416, -118.4085],
+    [37.6214, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line,{
+    color:"yellow",
+    dasharray:"4"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -24,6 +38,8 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city +"," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map)
    });
+
+
 
 
 // Then we add our 'graymap' tile layer to the map.
